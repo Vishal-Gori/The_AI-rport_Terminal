cecf95dd78b33ca0694aef8772e8ef006755aafe
Vishal-Gori <vishalgori2208@gmail.com>
2024-07-10 01:20:55 +0530

    Landing Page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,10 @@
 import { Text, View } from "react-native";
+import Login from '../components/Login'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{
-        fontSize:30,
-        fontFamily:'outfit-bold'
-      }}>Welcome to The AI-rport Terminal.</Text>
+    <View>
+      <Login/>
     </View>
   );
 }
